d2cd3231c298e4ac8e58f98c3863d8e5a7738348
Pedro Lambarri <plambarri@gmail.com>
2023-01-30 16:10:07 +0100

    promise-with-alert

--- src/index.js
+++ src/index.js
@@ -1,1 +1,45 @@
 import "./css/style.css"; //importing css
+
+const hiddenMessgae = document.querySelector(".modal");
+const span = document.querySelector(".close");
+let btn = document.querySelector(".btn");
+
+const showMessage = () => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(hiddenMessgae.style.display = "block");
+        }, 60000);
+    });
+};
+
+const printtingMessage = async () => {
+    let result = await showMessage();
+}
+
+printtingMessage()
+
+
+span.style.cursor = "pointer";
+
+span.addEventListener("click", () => {
+    hiddenMessgae.style.display = "none";
+})
+
+
+//BONUS-2
+
+const continueButton = () => {
+    return new Promise((resolve) => {
+        btn.addEventListener("animationend", () => {
+            resolve(btn.style.background = "red", alert("Continue to suscribe"));
+        })
+    });
+};
+
+const resultPromise = async () => {
+
+    let result = await continueButton();
+    
+};
+
+resultPromise();
